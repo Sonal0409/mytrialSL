3b3350348cd62639ba83903fd973e756cf5ff900
sonal0409 <mittal.sonal04@gmail.com>
2022-03-03 02:44:58 +0000

    new commit

---
+++ src/test/java/myseleniumScripts/Script1.java
@@ -0,0 +1,23 @@
+package myseleniumScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Script1 {
+
+	@Test
+	public void testcase1() {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","/home/mittalsonal04gm/Downloads/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://en.wikipedia.org/wiki/Main_Page");
+
+	}
+
+}
